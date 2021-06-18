1f37f1bfc5d47560279fd2c2d140b31c335dfd63
xdnroot <xdnroot@gmail.com>
2021-06-18 15:46:37 +0700

    add backup-multiple.py

---
+++ paramiko/backup-multiple.py
@@ -0,0 +1,28 @@
+import paramiko
+import time
+
+ip_addresses = ["10.1.4.100", "10.1.4.101", "10.1.4.102"]
+username = "cisco"
+password = "cisco123"
+
+for ip_address in ip_addresses:
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=ip_address,username=username,password=password, look_for_keys=False)
+
+    print ("Success login to {0}".format(ip_address))
+    conn = ssh_client.invoke_shell()
+
+    conn.send("terminal length 0\n")
+    conn.send("show run\n")
+    time.sleep(3)
+
+    output = conn.recv(65535).decode("utf-8")
+
+    output_file=open("{0}.cfg".format(ip_address), "w")
+    output_file.write(output)
+    output_file.close()
+
+    print("Config in {0} saved!!".format(ip_address))
+
+    ssh_client.close()
